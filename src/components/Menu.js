4b3e5c37717893b39a79fac7c024fd81ae5f6cb7
Camila Maraschin <camilapb11@hotmail.com>
2022-10-31 16:44:02 -0300

    sexto-projeto

---
+++ src/components/Menu.js
@@ -0,0 +1,34 @@
+import NavHeader from "./NavHeader";
+import { Link } from "react-router-dom";
+
+export default function Menu() {
+  return (
+    <nav id="menu" className="navbar navbar-default navbar-fixed-top">
+      <div className="container">
+        <NavHeader />
+        <div
+          className="collapse navbar-collapse"
+          id="bs-example-navbar-collapse-1"
+        >
+          <ul className="nav navbar-nav navbar-right">
+            <li>
+              <Link to="/features">Features</Link>
+            </li>
+            <li>
+              <Link to="/services">Services</Link>
+            </li>
+            <li>
+              <Link to="/portfolio">Gallery</Link>
+            </li>
+            <li>
+              <Link to="/testimonials">Testimonials</Link>
+            </li>
+            <li>
+              <Link to="/team">Team</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
